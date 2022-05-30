46cb6ff178af234451dfa25b677bd2e0e71009a3
king ali <hay19732000@outlook.com>
2022-05-30 18:05:20 +0300

    make widget function

--- lib/main.dart
+++ lib/main.dart
@@ -7,24 +7,64 @@ void main() {
 
 class Music extends StatelessWidget {
   const Music({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.white,
-        body: SafeArea(
-          child: Center(
-            child: TextButton(
-              onPressed: () {
-                final player = AudioCache();
-                player.play('note3.wav');
-              },
-              child: Text('note1.wav'),
+        backgroundColor: Colors.black,
+        body: SafeArea(child: PlayMusic()),
+      ),
+    );
+  }
+}
+
+class PlayMusic extends StatefulWidget {
+  const PlayMusic({Key? key}) : super(key: key);
+
+  @override
+  State<PlayMusic> createState() => _PlayMusicState();
+}
+
+class _PlayMusicState extends State<PlayMusic> {
+  String fileName = '';
+  final soundPlayer = AudioCache();
+  void playSound(String file) {
+    soundPlayer.play(file);
+  }
+
+  Expanded makeKey(String fileName, Color myColor) {
+    return Expanded(
+      child: Container(
+        color: myColor,
+        child: TextButton(
+          onPressed: () {
+            playSound(fileName);
+          },
+          child: const Text(
+            '',
+            style: TextStyle(
+              color: Colors.blue,
+              fontSize: 50,
             ),
           ),
         ),
       ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        makeKey('note1.wav', Colors.red),
+        makeKey('note2.wav', Colors.orange),
+        makeKey('note3.wav', Colors.yellow),
+        makeKey('note4.wav', Colors.green),
+        makeKey('note5.wav', Colors.teal),
+        makeKey('note6.wav', Colors.blue),
+        makeKey('note7.wav', Colors.purple),
+      ],
     );
   }
 }
